c711dcb55a11472386ec2ef849cb8aad919b7f8c
jakub-szewczyk <jakub.szewczyk.dev@gmail.com>
2024-01-14 18:40:07 +0100

    update telescope config

--- nvim/lua/jakubszewczyk/telescope.lua
+++ nvim/lua/jakubszewczyk/telescope.lua
@@ -3,3 +3,61 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+-- File and text search in hidden files and directories
+local telescope = require("telescope")
+local telescopeConfig = require("telescope.config")
+
+-- Clone the default Telescope configuration
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+-- I want to search in hidden/dot files.
+table.insert(vimgrep_arguments, "--hidden")
+-- I don't want to search in the `.git` directory.
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
+telescope.setup({
+	defaults = {
+		-- `hidden = true` is not supported in text grep commands.
+		vimgrep_arguments = vimgrep_arguments,
+	},
+	pickers = {
+		find_files = {
+			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+		},
+	},
+})
+
+-- Falling back to find_files if git_files can't find a .git directory
+-- telescope-config.lua
+local M = {}
+
+-- We cache the results of "git rev-parse"
+-- Process creation is expensive in Windows, so this reduces latency
+local is_inside_work_tree = {}
+
+M.project_files = function()
+	local opts = {} -- define here if you want to define something
+
+	local cwd = vim.fn.getcwd()
+	if is_inside_work_tree[cwd] == nil then
+		vim.fn.system("git rev-parse --is-inside-work-tree")
+		is_inside_work_tree[cwd] = vim.v.shell_error == 0
+	end
+
+	if is_inside_work_tree[cwd] then
+		builtin.git_files(opts)
+	else
+		builtin.find_files(opts)
+	end
+end
+
+return M
+
+-- call via:
+-- :lua require"telescope-config".project_files()
+
+-- example keymap:
+-- vim.api.nvim_set_keymap("n", "<Leader><Space>", "<CMD>lua require'telescope-config'.project_files()<CR>", {noremap = true, silent = true})
